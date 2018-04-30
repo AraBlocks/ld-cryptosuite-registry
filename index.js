81a37a750f10b79981147a7050b22bd1b0815b24
Joseph Werle <joseph.werle@gmail.com>
2018-04-30 15:19:19 -0400

    refactor(index.js): Add MerkleProof2017

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const kEdDsaSAPublicKeySecp256k1 = 'EdDsaSAPublicKeySecp256k1'
 // https://w3c-dvcg.github.io/lds-rsa2018/
 const kRsaVerificationKey2018 = 'RsaVerificationKey2018'
 
+// https://w3c-dvcg.github.io/lds-merkleproof2017/
+const kMerkleProof2017 = 'MerkleProof2017'
+
 // ld-cryptosuite-registry set
 const registry = new Set([
   kEd25519VerificationKey2018,
   kEdDsaSAPublicKeySecp256k1,
   kRsaVerificationKey2018,
+  kMerkleProof2017,
 ])
 
 // exported constants
@@ -21,6 +25,7 @@ Object.assign(registry, {
   kEd25519VerificationKey2018,
   kEdDsaSAPublicKeySecp256k1,
   kRsaVerificationKey2018,
+  kMerkleProof2017,
 })
 
 // exported values
@@ -28,6 +33,7 @@ Object.assign(registry, {
   Ed25519VerificationKey2018: kEd25519VerificationKey2018,
   EdDsaSAPublicKeySecp256k1: kEdDsaSAPublicKeySecp256k1,
   RsaVerificationKey2018: kRsaVerificationKey2018,
+  MerkleProof2017: kMerkleProof2017,
 })
 
 // disable destructive methods
